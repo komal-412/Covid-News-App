dcad4b13138df35e80fdd27c8a4d4693d623fbda
Komal Sharma <komal0137.cse19@chitkara.edu.in>
2021-05-23 10:45:47 +0530

    Update Footer.js

--- Footer.js
+++ Footer.js
@@ -7,15 +7,15 @@ const Footer = () => {
         <div className="footer">
         <div className="iconContainer">
         
-        <a href="https://www.instagram.com/" target="__blanc" >
+        <a href="https://www.instagram.com/" target="__blank" >
           <i className="fab fa-instagram-square fa-2x"></i>
         </a>
 
-        <a href="https://www.twitter.com/" target="__blanc" >
+        <a href="https://www.twitter.com/" target="__blank" >
           <i className="fab fa-twitter fa-2x"></i>
         </a>
 
-        <a href="https://www.linkedin.com/" target="__blanc" >
+        <a href="https://www.linkedin.com/" target="__blank" >
           <i className="fab fa-linkedin fa-2x"></i>
         </a>
         
